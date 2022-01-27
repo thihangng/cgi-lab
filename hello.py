9006728bc240b991706316f2ab87540f032351b5
Hang Ngo <thngo@ualberta.ca>
2022-01-26 18:23:20 -0700

    lab 3 code added

--- hello.py
+++ hello.py
@@ -11,17 +11,32 @@ import json
 ##  RUN  http://localhost:8080/hello.py with cgi_server.py run on the background
 
 # Q2 PRint ENV VARIABLES AS JSON
-print("Content-Type: application/json") # specify we want json 
-print()
-print(json.dumps(dict(os.environ), indent=2))  # Better formatting
+# print("Content-Type: application/json") # specify we want json 
+# print()
+# print(json.dumps(dict(os.environ), indent=2))  # Better formatting
 # RUN  http://localhost:8080/hello.py
 
+
 # Q2 PRINT QUERY PARAMETER IN HTML
-# print("Content-Type: text/html")  # Specify we want html
-# print()
-# print(f"<p>QUERY_STRING={os.environ['QUERY_STRING']}</p>")
+print("Content-Type: text/html")  # Specify we want html
+print()
+print(f"<p>QUERY_STRING={os.environ['QUERY_STRING']}</p>")
 
 #RUN http://localhost:8080/hello.py?thisisaquery to get the output
 
 
-#Q3  report the user’s browser in the HTML.
+# Q3  report the user’s browser in the HTML.
+print("Content-Type: text/html")  # Specify we want html
+print()
+print(f"<p>HTTP_USER_AGENT={os.environ['HTTP_USER_AGENT']}</p>")
+
+
+# How to read POST data (including forms!) in a CGI script:
+# import sys
+# posted_bytes = os.environ.get("CONTENT_LENGTH", 0)
+# if posted_bytes:
+#     posted = sys.stdin.read(int(posted_bytes))
+#     print(f"<p> POSTED: <pre>")
+#     for line in posted.splitlines():
+#         print(line)
+#     print("</pre></p>")
--- login.py
+++ login.py
@@ -1,5 +1,47 @@
+
+#!/usr/bin/env python3
+# import cgi
+# import cgitb
+# cgitb.enable()
+
+# from templates import login_page, secret_page, after_login_incorrect
+# import secret
+# import os
+# from http.cookies import SimpleCookie
+
+
+# # # CREATE LOGIN FORM
+
+# s = cgi.FieldStorage()
+# username = s.getfirst("username")
+# password = s.getfirst("password")
+
+# # Print to html
+# print("Content-type: text/html")
+# print()
+
+# #load login page, print user form info
+# if not username and not password:
+#     print(login_page())
+# elif username == secret.username and password == secret.password:
+#     print(secret_page(username, password))
+# else:
+#     print(after_login_incorrect())    
+
+
+
+# # RUN  http://localhost:8080/login.py to see the result
+
+
+
+
+# LOGIN WITH COOKIE
+
 import cgi
 import cgitb
+from errno import ENETRESET
+from webbrowser import BaseBrowser
+from xml.sax.handler import EntityResolver
 cgitb.enable()
 
 from templates import login_page, secret_page, after_login_incorrect
@@ -14,17 +56,46 @@ s = cgi.FieldStorage()
 username = s.getfirst("username")
 password = s.getfirst("password")
 
-# Print to html
-print("Content-type: text/html")
+# Check if correct login 
+form_o = username == secret.username and password == secret.password
+
+# SET UP COOKIE, simple cookie object from http cookie
+cookie = SimpleCookie(os.environ["HTTP_COOKIE"])
+cookie_username = None
+cookie_password = None
+if cookie.get("username"):  #check if cookie already exists
+    # Extract username from cookie in the browser
+    cookie_username = cookie.get("username").value
+if cookie.get("password"):
+    cookie_password = cookie.get("password").value
+
+
+# Check cookie user/pass = secret user/pass
+cookie_ok = cookie_username == secret.username and cookie_password == secret.password
+
+# SET user/pass to cookie user/pass
+if cookie_ok:
+    username = cookie_username
+    password = cookie_password
+
+print("Content-Type: text/html")
+if form_o:
+    print(f"set-cookie: username={username}")
+    print(f"set-cookie: password={password}")
 print()
 
-#load login page, print user form info
+# LOAD HTML PAGES
 if not username and not password:
     print(login_page())
 elif username == secret.username and password == secret.password:
     print(secret_page(username, password))
 else:
-    print(after_login_incorrect())    
-
+    print(after_login_incorrect())
+    
+# THIS WAY IF WE LOGIN, WHEN REFRESH, WE STAY LOGIN
 # RUN  http://localhost:8080/login.py to see the result
 
+# Q7: fake cookie
+# go to console in your BaseBrowser
+# javascript:document.cookie="{username} = {secret.username}"
+# DO THE SAME THING FOR PASSWORD
